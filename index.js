251ec3f8768257abcdda7c5cc1d10f8d574e2abf
rosivanov <ros.g6w@gmail.com>
2017-01-10 14:05:10 +0200

    run getRootSize() once #1

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const postcss = require('postcss');
+const getRootSize = require('./lib/getRootSize');
 const pxToEm = require('./lib/pxToEm');
 
 const defaults = {
   rootSelector: ':root',
   unit: 'pe',
-  fontSize: null
+  fontSize: 16
 };
 
 module.exports = postcss.plugin('postcss-pe', (opts) => {
-  opts = opts || {};
-  opts = Object.assign(defaults, opts);
+    opts = opts || {};
+    const userFontSize = opts.fontSize;
+    opts = Object.assign(defaults, opts);
 
   return (css) => {
+    const rootFontSize = userFontSize || getRootSize(css, opts) || opts.fontSize;
     const peReg = new RegExp('(\\d*\\.?\\d+)(?:\\/)?(\\d*\\.?\\d+)?' + opts.unit, 'gi');
 
     css.replaceValues(peReg, {fast: opts.unit}, (val) => {
-      return pxToEm(css, val, opts);
+      return pxToEm(val, rootFontSize);
     });
   };
 });
